a974e121e3a042c4235e9ce8b2d8852d1bc1db45
anand2694 <as926846@gmail.com>
2017-09-27 23:49:26 +0530

    static templete added

---
+++ static-templates/auto-landing-page/js/main.js
@@ -0,0 +1,21 @@
+
+
+
+$(function(){
+
+//   window.setTimeout(function(){
+//     $('.account-ring-wrap').addClass('orbit-animation');
+//     $('.animation-account-icon').fadeIn();
+//
+// }, 4000);
+//   $(window).scroll(function() {
+//     if($(this).scrollTop()>5) {
+//         $( ".automotive-navbar" ).addClass("fixed-navbar");
+//     } else {
+//         $( ".automotive-navbar" ).removeClass("fixed-navbar");
+//     }
+// });
+
+
+
+});
